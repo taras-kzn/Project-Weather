760e27c4f4fdc369df2ecd2ec5d973ba28c0c1f9
denis <9172885049@mail.ru>
2020-05-27 17:31:23 +0300

    test2

--- Project-Weather/MyCities/View/MyCitiesTableViewCell.swift
+++ Project-Weather/MyCities/View/MyCitiesTableViewCell.swift
@@ -16,6 +16,10 @@ class MyCitiesTableViewCell: UITableViewCell {
         super.awakeFromNib()
         
         
+        
+        
+        
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
