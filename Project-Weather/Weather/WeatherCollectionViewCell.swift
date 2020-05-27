3b9d340549b6fb3dbc3bcc0228d72097beba11e4
denis <9172885049@mail.ru>
2020-05-27 22:27:35 +0300

    Cделал custom view для WeatherCollectionView

--- Project-Weather/Weather/WeatherCollectionViewCell.swift
+++ Project-Weather/Weather/WeatherCollectionViewCell.swift
@@ -11,8 +11,24 @@ import UIKit
 
 class WeatherCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet var weatherView: UIView!
     @IBOutlet weak var weatherLabel: UILabel!
     @IBOutlet weak var iconImage: UIImageView!
     @IBOutlet weak var timeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        configView()
+    }
+    
+    func configView() {
+        weatherView.layer.cornerRadius = 30
+        weatherView.layer.shadowOpacity = 0.7
+        weatherLabel.clipsToBounds = true
+        weatherLabel.backgroundColor = .white
+        weatherLabel.layer.cornerRadius = 30
+        iconImage.layer.cornerRadius = 15
+    }
     
 }
