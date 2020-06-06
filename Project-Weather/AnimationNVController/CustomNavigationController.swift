69e71e42b31f934a0d87e6b14a9ac00512a73f61
denis <9172885049@mail.ru>
2020-06-06 10:53:13 +0300

    Сделал Анимацию переходов UINavigationController и Интерактивное закрытие экрана

---
+++ Project-Weather/AnimationNVController/CustomNavigationController.swift
@@ -0,0 +1,39 @@
+//
+//  CustomNavigationController.swift
+//  Project-Weather
+//
+//  Created by admin on 05.06.2020.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomNavigationController: UINavigationController, UINavigationControllerDelegate {
+    
+    let interactiveTransition = CustomInteractiveTransition()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+    }
+    
+    func navigationController(_ navigationController: UINavigationController,
+                              interactionControllerFor animationController: UIViewControllerAnimatedTransitioning)
+                              -> UIViewControllerInteractiveTransitioning? {
+        return interactiveTransition.hasStarted ? interactiveTransition : nil
+    }
+    
+    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        if operation == .push {
+            self.interactiveTransition.viewController = toVC
+            return CustomPushAnimator()
+        } else if operation == .pop {
+            if navigationController.viewControllers.first != toVC {
+                self.interactiveTransition.viewController = toVC
+            }
+            return CustomPopAnimator()
+        }
+        return nil
+    }
+}
